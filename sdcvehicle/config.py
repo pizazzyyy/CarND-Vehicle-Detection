81d30c3f7bcf7f5197317da3a2926a0e459ac0cc
YY <yydiligence@gmail.com>
2017-02-14 21:36:48 -0800

    Add reference code
    https://github.com/dolaameng/CarND-Vehicle-Detection

---
+++ sdcvehicle/config.py
@@ -0,0 +1,8 @@
+"""
+Configuration of `sdcvehicle` package, including training data and etc.
+"""
+
+vehicle_image_files = "./data/vehicles/*/*.png"
+nonvehicle_image_files = "./data/non-vehicles/*/*.png"
+
+model_file = "./model.pkl"
